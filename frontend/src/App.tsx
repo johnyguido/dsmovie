1a16ac54fd90530ed45aa5e9064407f132e692a8
johnyguido <johnyguido@hotmail.com.br>
2022-01-10 16:59:17 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
-
+import Form from "./pages/form";
+import Listing from "./pages/listing";
 
 function App() {
   return (
-   <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
